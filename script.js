7be4aa7106d1063702469860ff26d8a4f213130b
Sigrik <sigrikpoczta@gmail.com>
2024-02-08 16:47:39 +0100

    Refactor the modal to use objects

--- script.js
+++ script.js
@@ -1,16 +1,53 @@
-function showModal(title, description) {
+const modals = {
+  modal1: {
+    isOpen: false,
+    title: "Modal 1",
+    body: "Text of modal 1",
+    onClose: () => {
+      console.log("modal 1 was closed");
+    },
+    onSubmit: () => {
+      console.log("modal 2 was submitted");
+    },
+  },
+  modal2: {
+    isOpen: false,
+    title: "Modal 2",
+    body: "Text of modal 2",
+    onClose: () => {
+      console.log("modal 2 was closed");
+    },
+    onSubmit: () => {
+      console.log("modal 2 was submitted");
+    },
+  },
+  modal3: {
+    isOpen: false,
+    title: "Modal 3",
+    body: "Text of modal 3",
+    onClose: () => {
+      console.log("modal 3 was closed");
+    },
+    onSubmit: () => {
+      console.log("modal 3 was submitted");
+    },
+  },
+};
+
+function showModal(input) {
   const modalWrapper = document.createElement("div");
   const modal = document.createElement("div");
   const modalTitle = document.createElement("h2");
   const modalDescription = document.createElement("p");
   const modalExit = document.createElement("div");
   const modalButton = document.createElement("button");
-  modal.classList.add("modal");
   modalWrapper.classList.add("modal-wrapper");
-  modalTitle.innerHTML = title;
-  modalDescription.innerHTML = description;
+  modal.classList.add("modal");
+  modalTitle.innerHTML = input.title;
+  modalDescription.innerHTML = input.body;
+  modalButton.classList.add("modal-button");
   modalButton.innerHTML = "Accept";
-  modalExit.classList.add("modalExit");
+  modalExit.classList.add("modal-exit");
   modalExit.innerHTML = `&times;`;
   modal.appendChild(modalTitle);
   modal.appendChild(modalDescription);
@@ -18,38 +55,61 @@ function showModal(title, description) {
   modal.appendChild(modalExit);
   modalWrapper.appendChild(modal);
   document.getElementById("container").appendChild(modalWrapper);
-  modalExit.addEventListener("click", (e) => {
+
+  const acceptListener = function accept(e) {
     modalWrapper.remove();
-  });
-  window.addEventListener("keydown", (e) => {
+    input.onSubmit();
+    modalButton.removeEventListener("click", acceptListener);
+    modalExit.removeEventListener("click", exitListener);
+    window.removeEventListener("keydown", escapeListener);
+    window.removeEventListener("click", targetExitListener);
+  };
+
+  const exitListener = function exit(e) {
+    modalWrapper.remove();
+    input.onClose();
+    modalButton.removeEventListener("click", acceptListener);
+    modalExit.removeEventListener("click", exitListener);
+    window.removeEventListener("keydown", escapeListener);
+    window.removeEventListener("click", targetExitListener);
+  };
+
+  const escapeListener = function escape(e) {
     if (e.key === "Escape") {
       modalWrapper.remove();
+      input.onClose();
+      modalButton.removeEventListener("click", acceptListener);
+      modalExit.removeEventListener("click", exitListener);
+      window.removeEventListener("keydown", escapeListener);
+      window.removeEventListener("click", targetExitListener);
     }
-  });
-  window.addEventListener("click", (e) => {
+  };
+
+  const targetExitListener = function targetExit(e) {
     if (e.target === modalWrapper) {
       modalWrapper.remove();
+      input.onClose();
+      modalButton.removeEventListener("click", acceptListener);
+      modalExit.removeEventListener("click", exitListener);
+      window.removeEventListener("keydown", escapeListener);
+      window.removeEventListener("click", targetExitListener);
     }
-  });
+  };
+
+  modalButton.addEventListener("click", acceptListener);
+  modalExit.addEventListener("click", exitListener);
+  window.addEventListener("keydown", escapeListener);
+  window.addEventListener("click", targetExitListener);
 }
 
 document.getElementById("modalButton-1").addEventListener("click", (e) => {
-  showModal(
-    "Modal 1",
-    "Description of the first modal, some more text goes here"
-  );
+  showModal(modals.modal1);
 });
 
 document.getElementById("modalButton-2").addEventListener("click", (e) => {
-  showModal(
-    "Modal 2",
-    "Description of the second modal, some more text goes here"
-  );
+  showModal(modals.modal2);
 });
 
 document.getElementById("modalButton-3").addEventListener("click", (e) => {
-  showModal(
-    "Modal 3",
-    "Description of the third modal, some more text goes here"
-  );
+  showModal(modals.modal3);
 });
